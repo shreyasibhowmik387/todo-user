1afa90db3d118cc7cfb761157075cdbbc8f4f5f3
shreyasi <shreyasibhowmik387@gmail.com>
2025-11-04 17:04:55 +0530

    getAll, delete done

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -1,43 +1,102 @@
 import todoSchema from "../models/todoSchema.js";
 
 export const addTodo = async (req, res) => {
-  try {
-    const { title } = req.body;
+    try {
+        const { title } = req.body;
 
-    const existing = await todoSchema.findOne({
-      title: title,
-      userId: req.userId,
-    });
+        const existing = await todoSchema.findOne({
+            title: title,
+            userId: req.userId,
+        });
 
-    if (existing) {
-      return res.status(400).json({
-        success: false,
-        message: "Title already exists",
-      });
+        if (existing) {
+            return res.status(400).json({
+                success: false,
+                message: "Title already exists",
+            });
+        }
+
+        const data = await todoSchema.create({
+            title,
+            userId: req.userId,
+        });
+
+        if (!data) {
+            return res.status(500).json({
+                success: false,
+                message: "Todo not created",
+            });
+        }
+
+        return res.status(201).json({
+            success: true,
+            message: "Todo created successfully",
+            data,
+        });
+    } catch (error) {
+        console.error("Error in addNote:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Could not access",
+        });
     }
-
-    const data = await todoSchema.create({
-      title,
-      userId: req.userId,
-    });
-
-    if (!data) {
-      return res.status(500).json({
-        success: false,
-        message: "Todo not created",
-      });
-    }
-
-    return res.status(201).json({
-      success: true,
-      message: "Todo created successfully",
-      data,
-    });
-  } catch (error) {
-    console.error("Error in addNote:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Could not access",
-    });
-  }
 };
+
+
+export const getAllTodo = async (req, res) => {
+    try {
+
+        const data = await todoSchema.find({ userId: req.userId });
+
+        if (!data) {
+            return res.status(500).json({
+                success: false,
+                message: "Todo not fetched",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Todo fetched successfully",
+            data,
+        });
+    } catch (error) {
+        console.error("Error in addNote:", error);
+        return res.status(400).json({
+            success: false,
+            message: "Todo not fetched",
+        });
+    }
+};
+
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const todoId = req.params.id;
+        const data = await todoSchema.findByIdAndDelete({ userId: req.userId, _id: todoId })
+
+        if (data) {
+            return res.status(200).json({
+                success: true,
+                message: "Todo deleted successfully",
+                data
+            });
+        }
+        else {
+            return res.status(404).json({
+                success: true,
+                message: "Todo not found",
+                data,
+            });
+        }
+
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Todo not deleted"
+        });
+    }
+
+}
+
--- src/route/todoRoute.js
+++ src/route/todoRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addTodo } from "../controller/todoController.js";
+import { addTodo, deleteTodo, getAllTodo } from "../controller/todoController.js";
 import { hasToken } from "../middleware/hasToken.js";
 
 const todoRoute = express.Router();
 
-todoRoute .post("/create", hasToken, addTodo);
+todoRoute.post("/create", hasToken, addTodo);
+todoRoute.get("/getAll", hasToken, getAllTodo);
+todoRoute.delete("/delete/:id", hasToken, deleteTodo);
+
 export default todoRoute ;
