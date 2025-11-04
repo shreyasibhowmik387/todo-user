7249863a43d8b9f59a1ad2cdb5c72213a88d2cc4
shreyasi <shreyasibhowmik387@gmail.com>
2025-11-04 18:00:16 +0530

    crud operation done

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -100,3 +100,53 @@ export const deleteTodo = async (req, res) => {
 
 }
 
+
+export const todoUpdate = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const todoId = req.params.id;
+
+    // Find the existing note first
+    const data = await todoSchema.findOne({
+      userId: req.userId,
+      _id: todoId,
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found",
+      });
+    }
+
+    // Check if another note with the same title exists
+    const existing = await todoSchema.findOne({
+        title:title,
+        userId: req.userId,
+      })
+
+    if (existing) {
+      return res.status(400).json({
+        success: false,
+        message: "Title already exists",
+      });
+    }
+
+    // Update and save the note
+    data.title = title;
+    data.updatedAt = Date.now();
+    await data.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Todo updated successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Could not update todo",
+    });
+  }
+};
--- src/route/todoRoute.js
+++ src/route/todoRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addTodo, deleteTodo, getAllTodo } from "../controller/todoController.js";
+import { addTodo, deleteTodo, getAllTodo, todoUpdate } from "../controller/todoController.js";
 import { hasToken } from "../middleware/hasToken.js";
 
 const todoRoute = express.Router();
@@ -7,5 +7,6 @@ const todoRoute = express.Router();
 todoRoute.post("/create", hasToken, addTodo);
 todoRoute.get("/getAll", hasToken, getAllTodo);
 todoRoute.delete("/delete/:id", hasToken, deleteTodo);
+todoRoute.put("/update/:id", hasToken, todoUpdate);
 
 export default todoRoute ;
